24012c052b25db50b2324ee73e0da4ad916dc846
andre <andre.vd.laan@hotmail.com>
2016-09-14 16:18:17 +0200

    classes

---
+++ src/Module/InviteRevoke.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andre
+ * Date: 14-9-2016
+ * Time: 15:25
+ */
+
+namespace RoomManager\Module;
+
+
+class InviteRevoke
+{
+
+}
---
+++ src/Module/Login.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andre
+ * Date: 14-9-2016
+ * Time: 15:26
+ */
+
+namespace RoomManager\Module;
+
+
+class Login
+{
+
+}
