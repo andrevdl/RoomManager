66182f99758bf1917c57b877c79eced50ba1f64d
GarciaSiego <gsrijnders@hotmail.com>
2016-09-14 19:05:29 +0200

    GETS

--- src/Module/RoomReserve.php
+++ src/Module/RoomReserve.php
@@ -9,7 +9,69 @@
 namespace RoomManager\Module;
 
 
-class RoomReserve
-{
+use RoomManager\Core\Http\HttpResponse;
+use RoomManager\Core\Http\Request;
+use RoomManager\Core\Http\Response;
+use RoomManager\Core\SQL;
 
+class RoomReserve implements HttpResponse
+{
+    /**
+     * @var SQL
+     */
+    private $sql;
+
+    public function init(SQL $SQL)
+    {
+        $this->sql = $SQL;
+    }
+
+    public function doGet(Request $request, Response $response)
+    {
+        $response->setCode(400);
+        return;
+    }
+
+    public function doPost(Request $request, Response $response)
+    {
+        $vars = [
+            "room",
+            "date",
+            "start",
+            "end",
+            "description"
+        ];
+
+        foreach ($vars as $var) {
+            if (!isset($_POST[$var])) {
+                $response->setCode(400);
+                return;
+            }
+        }
+
+        $status = $this->sql->insert(
+            "reservations",
+            [
+                "room_id" => $_POST["room"],
+                "date" => $_POST["date"],
+                "start_time" => $_POST["start"],
+                "end_time" => $_POST["end"],
+                "description" => $_POST["description"]
+            ],
+            ["%d", "%s", "%s", "%s", "%s"]
+        );
+        //Add reservation parameter to request
+        $request->addQuery("reservation", $this->sql->lastInsertId());
+
+        if ($status === false) {
+            $response->setCode(400);
+            return;
+        }
+
+        if (isset($_POST["ids"])) {
+            $c = new ReservationInvite();
+            $c->init($this->sql);
+            $c->doPost($request, $response);
+        }
+    }
 }
